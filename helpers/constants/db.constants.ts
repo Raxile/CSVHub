80e344286a793258384d3a76cf6bda36305e4aab
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2024-02-04 19:14:21 +0530

    development - add db connect ts file

--- helpers/constants/db.constants.ts
+++ helpers/constants/db.constants.ts
@@ -3,3 +3,39 @@ export const DB_ERROR_MESSAGE: {
 } = {
   NO_ENV: 'Invalid/Missing environment variable: "MONGODB_URI"',
 };
+
+export const SCHEMA_VALIDATION_MESSAGE: {
+  NAME: {
+    REQUIRED: string;
+    MAX_LIMIT: string;
+  };
+  EMAIL: {
+    REQUIRED: string;
+  };
+  DATE_OF_BIRTH: {
+    REQUIRED: string;
+  };
+  ADDRESS: {
+    REQUIRED: string;
+  };
+  COUNTRY: {
+    REQUIRED: string;
+  };
+} = {
+  NAME: {
+    REQUIRED: 'Please provide a name for this Student.',
+    MAX_LIMIT: 'Name cannot be more than 60 characters.',
+  },
+  EMAIL: {
+    REQUIRED: 'Please provide a email.',
+  },
+  DATE_OF_BIRTH: {
+    REQUIRED: 'Please provide a Date of birth.',
+  },
+  ADDRESS: {
+    REQUIRED: 'Please provide a address.',
+  },
+  COUNTRY: {
+    REQUIRED: 'Please provide a country.',
+  },
+};
---
+++ helpers/lib/dbConnect.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { DB_ERROR_MESSAGE } from '../constants/db.constants';
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(DB_ERROR_MESSAGE.NO_ENV);
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
