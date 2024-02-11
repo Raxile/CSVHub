3d30459dfce2d5f4ff4d104b7b61b88464ac1ec8
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2024-02-11 14:23:47 +0530

    api-integration - add colors in root variables - add Header and footer - create common button component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-[var(--primary)] text-white">
+    <div className="container mx-auto flex justify-center items-center text-xs h-7">
+      "Connecting Data, Empowering Decisions - Your CSV Hub"
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ components/form-element/Button.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+interface Props {
+  children?: ReactNode;
+  className?: string;
+  disabled?: boolean;
+  // any props that come into the component
+}
+
+const Button = ({ children, className, disabled = false }: Props) => (
+  <button
+    className={`  flex justify-center items-center gap-1 hover:opacity-85 disabled:bg-[var(--disabled)] drop-shadow-md active:drop-shadow-sm   disabled:pointer-events-none ${className}  font-bold py-2 px-4 rounded`}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
